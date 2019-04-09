33810fb217452bccbb0e14798225acf94fbcea17
Ran Ding <dingran.nju@gmail.com>
2019-04-09 07:44:15 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const keys = require("./keys");
+
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+pgClient.on("error", () => console.log("Lost PG connection"));
+
+pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)").catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require("redis");
