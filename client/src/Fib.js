e554c85d72721557226f33caa56a4139366506e0
Ran Ding <dingran.nju@gmail.com>
2019-04-14 10:47:42 -0400

    add ingress

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: ''
+    index: ""
   };
 
   componentDidMount() {
@@ -14,12 +14,12 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get('/api/values/current');
+    const values = await axios.get("/api/values/current");
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
+    const seenIndexes = await axios.get("/api/values/all");
     this.setState({
       seenIndexes: seenIndexes.data
     });
@@ -28,14 +28,18 @@ class Fib extends Component {
   handleSubmit = async event => {
     event.preventDefault();
 
-    await axios.post('/api/values', {
+    await axios.post("/api/values", {
       index: this.state.index
     });
-    this.setState({ index: '' });
+    this.setState({ index: "" });
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    try {
+      return this.state.seenIndexes.map(el => el.number).join(", ");
+    } catch (error) {
+      return "";
+    }
   }
 
   renderValues() {
